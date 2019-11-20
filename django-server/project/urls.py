9e099e5914db443ccc2d26495e54a5c06ae60f7c
Jakob Blume <jakob@entwicklerheld.de>
2019-11-20 23:14:04 +0100

    added last changes to support minimal docker-compose file

--- django-server/project/urls.py
+++ django-server/project/urls.py
@@ -1,8 +1,4 @@
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("frontend.urls")),
-    path("api/", include("actions.urls")),
-]
+urlpatterns = [path("admin/", admin.site.urls), path("api/", include("actions.urls"))]
