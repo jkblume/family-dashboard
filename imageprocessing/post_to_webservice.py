6e50bef98e9e7a0f6a1d931f96682f06a45ee733
Jakob Blume <jakob@entwicklerheld.de>
2019-11-28 20:37:48 +0100

    fixed missing packages in requirements txt and fixed s<ntax error

--- imageprocessing/post_to_webservice.py
+++ imageprocessing/post_to_webservice.py
@@ -3,7 +3,7 @@ import requests
 
 def send_request(person_image, predicted_class):
     payload = {
-        "person_id": predicted_class
+        "person_id": predicted_class,
         "image": person_image
     }
     response = requests.post("http://localhost:8000/api/detected_person", json=payload)
