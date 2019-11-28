25efe8376a94206bf5ac038b596ca9ee3204669f
Maximilian Dargatz <max.dargatz@de.ibm.com>
2019-11-28 17:33:11 +0100

    post vorbereitet

--- imageprocessing/post_to_webservice.py
+++ imageprocessing/post_to_webservice.py
@@ -1,9 +1,10 @@
 
 import requests
 
-def send_request():
+def send_request(person_image, predicted_class):
     payload = {
-        "person_id": "7b50b7f5-43a3-4ce7-b3c2-15cb554ce085"
+        "person_id": predicted_class
+        "image": person_image
     }
     response = requests.post("http://localhost:8000/api/detected_person", json=payload)
     
